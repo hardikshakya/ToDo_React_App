f63c45584a532c4fd938050edcfc34e64facb6f2
Hardik Shakya <hardik.shakya@HardikS.local>
2019-04-25 16:17:16 +0530

    Created Todos.js components and it get todo list from App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello From Hardik.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Todos from './components/Todos'
+class App extends Component {
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Book Show',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Place Order',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'Meet friends',
+            completed: false
+        }
+    ]
+  }
+  render(){
+    // console.log(this.state.todos);
+    return (
+      <div className="App">
+        {/* <h1>App</h1> */}
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+class Todos extends Component {
+
+    render(){
+        // console.log(this.props.todos);
+
+        return this.props.todos.map((todo) => (
+            <h3>{todo.title}</h3>
+        ));
+                
+        // return (
+        //     <div>
+        //         <h1>Todos</h1>
+        //     </div>
+        // );
+    }  
+}
+
+export default Todos;
